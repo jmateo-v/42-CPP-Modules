434866ed1c44ee19c06fe9167f340c8da5c0c0ee
jmateo-v <jmateov5@gmail.com>
2025-11-25 14:50:27 +0000

    Module00/ex00 added

---
+++ Module00/ex00/megaphone.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dogs <dogs@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/25 14:05:10 by dogs              #+#    #+#             */
+/*   Updated: 2025/11/25 14:31:35 by dogs             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <cctype>
+
+int	main(int argc, char **argv)
+{
+	(void)argv;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			std::string arg = argv[i];
+			for (size_t j = 0; j < arg.length(); j++)
+			{
+				std::cout << (char)std::toupper(arg[j]);
+			}
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+}
