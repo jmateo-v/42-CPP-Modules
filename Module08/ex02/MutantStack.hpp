64d2e23bafa72257c896616f11079fe709870973
jmateo-v <jmateov5@gmail.com>
2026-02-24 17:22:52 +0100

    Layout for Module09/, Module08 modification for subject compliance

--- Module08/ex02/MutantStack.hpp
+++ Module08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: jmateo-v <jmateo-v@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 16:15:30 by jmateo-v          #+#    #+#             */
-/*   Updated: 2026/02/24 16:29:33 by jmateo-v         ###   ########.fr       */
+/*   Updated: 2026/02/24 17:22:21 by jmateo-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,15 @@ template <typename T>
 class MutantStack : public std::stack<T>
 {
 public:
+	MutantStack() : std::stack<T>() {}
+	MutantStack(const MutantStack& other) : std::stack<T>(other) {}
+	MutantStack& operator=(const MutantStack& other)
+	{
+		std::stack<T>::operator=(other);
+		return *this;
+	}
+	~MutantStack() {}
+	
 	typedef typename std::stack<T>::container_type::iterator iterator;
 	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
 
